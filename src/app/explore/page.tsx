f93f8693a7459105314cc356dfd7dd6b8a8acacf
DarkerCyan <k1914803@students.katyisd.org>
2024-05-26 20:55:42 -0500

    overflowhidden and padding

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -6,10 +6,10 @@ import Navbar from '@/components/ui/navbar';
 
 export default function Explore() {
     return (
-        <div className="relative min-h-screen w-screen overflow-x-hidden">  {/* Set relative positioning on outer div */}
+        <div className="relative min-h-screen w-screen overflow-hidden">  {/* Set relative positioning on outer div */}
             <BackgroundGradientAnimation className="h-full" />  {/* No need for z-index here */}
             <Navbar />
-            <div className="z-10 absolute flex h-screen min-screen flex-col items-center justify-center top-0 left-0 right-0 bottom-0 text-4xl text-center">
+            <div className="z-10 absolute flex h-screen min-screen flex-col items-center justify-center top-0 left-0 right-0 bottom-0 text-4xl text-center px-4">
                 <p>COME BACK SOON TO SEE OUR PUBLICATIONS</p>
                 <p>ETA: FIRST WEEK OF JUNE</p>
             </div>
